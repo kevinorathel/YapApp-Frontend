e9d4ee539a6ea3670f3a289aeab7ef6a0ba39966
kevin.orathel <kevin.orathel@gmail.com>
2023-04-26 02:30:32 +0530

    Feat: Added verification for signup

---
+++ backend/signup.js
@@ -0,0 +1,57 @@
+const signUp = document.querySelector(".btn-primary");
+
+signUp.addEventListener('click', (event) => {
+    
+    event.preventDefault();
+    const userName = document.querySelector('#userName').value;
+    const email = document.querySelector('#email').value;
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+    const password = document.querySelector('#password').value;
+    const confirmPassword = document.querySelector('#confirmPassword').value;
+
+    const flag = checker(userName, email, firstName, lastName, password, confirmPassword);
+    if(flag){
+        alert("Account created succesfully!");
+        window.location.href = "../Code/login.html";
+    }
+    else{
+        alert("Ran into an error. Please check your credentials.");
+    }
+});
+
+function checker(userName, email, firstName, lastName, password, confirmPassword){
+
+    if(userName === ""){
+        alert("Please enter a username.");
+        return false;
+    }
+    else if(email === ""){
+        alert("Please enter a email.");
+        return false;
+    }
+    else if(firstName === ""){
+        alert("Please enter a first name.");
+        return false;
+    }
+    else if(lastName === ""){
+        alert("Please enter a last name.");
+        return false;
+    }
+    else if(password === ""){
+        alert("Please enter a password.");
+        return false;
+    }
+    else if(confirmPassword === ""){
+        alert("Please re enter your password.");
+        return false;
+    }
+    else if(password != confirmPassword){
+        alert("Password and Confirm Password dont match.");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
